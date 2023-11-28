9f6ddfdf85f21cfeb1d44845286097ef840662c6
bagheera30 <rizkialfian30103@gmail.com>
2023-11-28 23:32:22 +0700

    routing:id produk

--- src/libs/api-libs.js
+++ src/libs/api-libs.js
@@ -5,3 +5,11 @@ export const getApiResponse = async (resource, query) => {
   const product = await response.json();
   return product;
 };
+
+export const getApiResponseById = async (resource, id) => {
+  const response = await fetch(
+    `${import.meta.env.PUBLIC_API_PRODUCT}/${resource}/${id}`
+  );
+  const product = await response.json();
+  return product;
+};
